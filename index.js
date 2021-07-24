18d981d4689180dc02ba5e8b5cc545240386dbaa
Safwan MI <saffanmd.cs19@bmsce.ac.in>
2021-07-24 22:42:41 +0530

    corrected cors link

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(cors()) // Use this after the variable declaration
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'https://placement-experience.herokuapp.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
